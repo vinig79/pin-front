88caf244cb6e696c80986fa7288c67d853ec4454
JoséZevs <100696361+Jose-Vitor-Sousa-Cardoso-Requena@users.noreply.github.com>
2023-08-14 23:46:00 -0300

    mudança no home

--- src/Views/Home/Home.jsx
+++ src/Views/Home/Home.jsx
@@ -5,6 +5,28 @@ export default function Home(){
     return(
         <>
             <MainNavBar/>
+            <header>
+                <h1>Cursos de Alfabetização</h1>
+                <p>Seja bem-vindo ao nosso site de cursos de alfabetização. Aqui você pode aprender a ler e escrever de forma fácil e divertida!</p>
+            </header>
+            
+            <main>
+                <section class="cursos">
+                <h2>Nossos Cursos</h2>
+                <ul>
+                    <li>Alfabetização 1</li>
+                    <li>Alfabetização 2</li>
+                    <li>Alfabetização 3</li>
+                    <li>Alfabetização 4</li>
+                </ul>
+                </section>
+            </main>
+            <div>
+
+            </div>
+            <footer>
+                <p>&copy; 2023 Cursos de Alfabetização</p>
+            </footer>
         </>
     );
 }
