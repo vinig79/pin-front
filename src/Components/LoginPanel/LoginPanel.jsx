01eb7a4ca9cdedbf850fca3bcfe1ec9799dca539
Vinicius groto <viniciusgroto@gmail.com>
2023-08-18 02:30:37 -0300

    register and login

--- src/Components/LoginPanel/LoginPanel.jsx
+++ src/Components/LoginPanel/LoginPanel.jsx
@@ -1,19 +1,30 @@
 import axios from "axios"
-import { useState} from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import styles from "./LoginPanel.scss";
 export default function LoginPanel(){
-    const navigate = useNavigate()
+    const navigate = useNavigate();
+    const [data, setData] = useState({email:'',password:''});
+    
+    const handleInput = (event) =>{
+        setData({...data, [event.target.name]: event.target.value});
+    };
+
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+        axios.post('http://localhost:5000/login',data)
+        navigate('/')
+    };
 
     return(
         <>
             <div className="LoginBody">
                 <div className="login-panel">
                     <h2>Login</h2>
-                    <form>
-                    <input type="text" class="login-input" placeholder="Usuário"/>
-                    <input type="password" class="login-input" placeholder="Senha"/>
+                    <form onSubmit={handleSubmit}>
+                    <input type="email" class="login-input" placeholder="Email" name='email' onChange={handleInput}/>
+                    <input type="password" class="login-input" placeholder="Senha"  name='password'onChange={handleInput} />
                     <button type="submit" class="login-button">Entrar</button>
                     <div class="registro">
                         <p>Não possui Conta?</p>
