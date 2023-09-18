2ff8c5e96dbb844199658850d793c027d62fe536
JoséZevs <100696361+Jose-Vitor-Sousa-Cardoso-Requena@users.noreply.github.com>
2023-09-17 21:27:00 -0300

    jogando na master

--- src/Views/Login/Login.jsx
+++ src/Views/Login/Login.jsx
@@ -8,7 +8,7 @@ export default function Login(){
     const navigate = useNavigate();
 
     const redirecionarParaOutraRota = () => {
-        navigate("/")
+        navigate("")
     };
 
     return(
--- src/Views/Register/Register.jsx
+++ src/Views/Register/Register.jsx
@@ -7,7 +7,7 @@ export default function Login(){
     const navigate = useNavigate();
 
     const redirecionarParaOutraRota = () => {
-        navigate("/")
+        navigate("")
     };
 
     return(
