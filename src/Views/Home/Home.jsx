2005f95a8bfec9ce26cc589e217a83f7e515c49b
JoséZevs <100696361+Jose-Vitor-Sousa-Cardoso-Requena@users.noreply.github.com>
2023-09-27 11:45:07 -0300

    mudanças no front

--- src/Views/Home/Home.jsx
+++ src/Views/Home/Home.jsx
@@ -4,45 +4,44 @@ import MainSideBar from '../../Components/MainSideBar/MainSideBar.jsx'
 import Lapis from '../../Assets/lápis.png'
 
 import styles from "./Home.scss";
-export default function Home(){
+export default function Home() {
     const navigate = useNavigate();
     let itens = []
-    for (let i = 1 ; i <=6 ; i++){
+    for (let i = 1; i <= 6; i++) {
         itens.push(<div key={i} className="container_ItemGridHome">
-            <img  src={require(`./Imagens/${i}.png`)} alt="" />
+            <img src={require(`./Imagens/${i}.png`)} alt="" />
         </div>)
     }
-    return(
+    return (
         <>
-            <MainNavBar/>
-            <MainSideBar/>
-            <header className='BarraHeaderHome'> 
-                
-            </header>
-            <div className='BodyHome'>
-                <main>
-                    <section className="cursos">
-                    <div className="container_GridHome">
-                    {itens.map((itens, index) => (
-                        <a onClick={() => {navigate('/Videos')}}>
-                            <div key={index} className="container_ItemGridHome">
-                            {itens}
+            <MainNavBar />
+            <MainSideBar />
+            <div className="MainBodyHome">
+                <header className='BarraHeaderHome'>
+
+                </header>
+                <div className='BodyHome'>
+                    <main>
+                        <section className="cursos">
+                            <div className="container_GridHome">
+                                {itens.map((itens, index) => (
+                                    <a className='BotaoAula' onClick={() => { navigate('/Videos') }}>
+                                        {itens}
+                                    </a>
+                                ))}
                             </div>
-                        </a>
-                        
-                    ))}
-                    </div>
-                    </section>
-                </main>
-            </div>  
-            <footer>
-                
-            </footer>
-            <div className='GridLapis'>
-                <img className="Lapis" src={Lapis} alt="" />
-                {/* <img className="Lapis" src={Lapis} alt="" />
-                <img className="Lapis" src={Lapis} alt="" />
-                <img className="Lapis" src={Lapis} alt="" /> */}
+                        </section>
+                    </main>
+                </div>
+                <footer>
+
+                </footer>
+                <div className='GridLapis'>
+                    <img className="Lapis" src={Lapis} alt="" />
+                    {/* <img className="Lapis" src={Lapis} alt="" />
+                            <img className="Lapis" src={Lapis} alt="" />
+                            <img className="Lapis" src={Lapis} alt="" /> */}
+                </div>
             </div>
         </>
     );
--- src/Views/Videos/Videos.jsx
+++ src/Views/Videos/Videos.jsx
@@ -9,9 +9,9 @@ export default function Video() {
         <>
         <body>
             
-            <div className="container">
+            <div className="ContainerAula">
                 <h2>Aula</h2>
-                <iframe width="560" height="315" src={data.url} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <iframe className='VideoAula' src={data.url} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </div>
         </body>
         </>
