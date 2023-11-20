84eb16dff251976e528cba471c2652d8b1899805
Vinicius groto <viniciusgroto@gmail.com>
2023-11-20 12:53:21 -0300

    login

--- src/Components/LoginPanel/LoginPanel.jsx
+++ src/Components/LoginPanel/LoginPanel.jsx
@@ -1,38 +1,74 @@
-import axios from "axios"
+import axios from "axios";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import styles from "./LoginPanel.scss";
-export default function LoginPanel(){
-    const navigate = useNavigate();
-    const [data, setData] = useState({email:'',password:''});
-    
-    const handleInput = (event) =>{
-        setData({...data, [event.target.name]: event.target.value});
-    };
+export default function LoginPanel() {
+  const navigate = useNavigate();
+  const [data, setData] = useState({ email: "", password: "" });
+  const [error, setError] = useState();
 
-    const handleSubmit = async (event) =>{
-        event.preventDefault();
-        const response = axios.post('https://pin-back.vercel.app/login',data)
-        navigate('/')
-    };
+  const handleInput = (event) => {
+    setData({ ...data, [event.target.name]: event.target.value });
+  };
 
-    return(
-        <>
-            <div className="LoginBody">
-                <div className="login-panel">
-                    <h2>Login</h2>
-                    <form onSubmit={handleSubmit}>
-                    <input type="email" className="login-input" placeholder="Email" name='email' onChange={handleInput} required/>
-                    <input type="password" className="login-input" placeholder="Senha"  name='password'onChange={handleInput} required/>
-                    <button type="submit" className="login-button">Entrar</button>
-                    <div className="registro">
-                        <p>Não possui Conta?</p>
-                        <button type="route" className="register-butto" onClick={ () =>{navigate('/Register')}}> Registrar</button>
-                    </div>
-                    </form>
-                </div>
-            </div>
-        </>
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const response = await axios.post(
+      "https://pin-back.vercel.app/login",
+      data
     );
+    if (response.data.error) {
+      setError(response?.data.error);
+    } else {
+      navigate("/");
+    }
+  };
+
+  return (
+    <>
+      <div className="LoginBody">
+        <div className="login-panel">
+          <h2>Login</h2>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="email"
+              className="login-input"
+              placeholder="Email"
+              name="email"
+              onChange={handleInput}
+              required
+            />
+            <div>
+              <input
+                type="password"
+                className="login-input"
+                placeholder="Senha"
+                name="password"
+                onChange={handleInput}
+                required
+              />
+							{error && <span style={{color:'red'}}> [{error}]</span>}
+            </div>
+            <button type="submit" className="login-button">
+              Entrar
+            </button>
+            <div className="registro">
+              <p>Não possui Conta?</p>
+              <button
+                type="route"
+                className="register-butto"
+                onClick={() => {
+                  navigate("/Register");
+                }}
+              >
+                {" "}
+                Registrar
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
